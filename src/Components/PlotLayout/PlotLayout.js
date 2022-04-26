6dbde57a0cb66f9dc473b31ceb1f12ea73a81072
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-04-27 00:31:56 +0530

    popup card added

--- src/Components/PlotLayout/PlotLayout.js
+++ src/Components/PlotLayout/PlotLayout.js
@@ -1,16 +1,11 @@
-import React from 'react'
-import './PlotLayout.css'
+import React,{useEffect,useState} from 'react'
+import './PlotLayout.css';
 
 function PlotLayout() {
+  const [card, setCard] = useState(false)
   let plot={}
-  // for(let i=0;i<6;i++){
-  //   for(let j=0;j<5;j++){
-  //     console.log(i,j);
-     
-  //   }
-  // }
+  let services = { "House": '1', "Restaurant": "2", "Gym": "3","Hospital":"4"};
   
-
   let App = () => {
     return (<table cellspacing="15" className='layout table-bordered'>
       {
@@ -20,8 +15,8 @@ function PlotLayout() {
               Array.from(Array(5), (a, j) => {
 
                 plot[i + "" + j]={'row':i,"column":j,plot:{}}
-                
-                return (<td className='p-2 table-cell ' onClick={()=>{console.log(i,j);}} id={`${i}${j}` } key={j}> <h2  className='text-center'>+</h2> </td>)
+
+                return (<td className='p-2 table-cell ' onClick={()=>{setCard(!card); console.log(i,j)}} id={`${i}${j}` } key={j}> <h2  className='text-center'>+</h2> </td>)
               })
             }
           </tr>)
@@ -30,12 +25,11 @@ function PlotLayout() {
     </table>)
     
   }
-  console.log(plot);
-
 
   return (
     <div className='layout-container'>
       <section>
+
         <div className='container'>
           <div className='row'>
             <div className='col-md-12'>
@@ -48,7 +42,7 @@ function PlotLayout() {
         <div className='container'>
           <div className='row mt-5 m-3'>
             <div className=' col-6 col-md-3 text-center'>
-              <h3 className='text-success'>House</h3>
+              <h3 className='text-success'>House </h3>
             </div>
             <div className='col-6 col-md-3 text-center'>
               <h3 className='text-warning'>Restaurant</h3>
@@ -68,6 +62,26 @@ function PlotLayout() {
             <div className='plot-container p-3'>
               <App />
             </div>
+          </div>
+        </div>
+      </div>
+     
+      <div className="modal fade show" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-modal="true" style={{display:"block"}}>
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              ...
+            </div>
+            {/* <div classNameName="modal-footer">
+              <button type="button" classNameName="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div> */}
           </div>
         </div>
       </div>
